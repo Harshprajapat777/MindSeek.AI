0113b3ffd470adf28890199e5d43d9c28370ec17
harsh prajapat <harshcdgi22@gmail.com>
2026-01-17 20:54:04 +0530

    Templates designed and Home screen

--- searchengine/searchengine/settings.py
+++ searchengine/searchengine/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "searchengine.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "searchengine" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
