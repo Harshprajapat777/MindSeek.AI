32b83a57999f4ed2d6d5e76b1f6fff5ed049c647
harsh prajapat <harshcdgi22@gmail.com>
2026-01-20 20:00:08 +0530

    Improved the Layers and API UI updated

--- searchengine/searchengine/urls.py
+++ searchengine/searchengine/urls.py
@@ -3,28 +3,23 @@ URL configuration for searchengine project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import search_views
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
+
+    # Main Pages
     path("", views.home, name="home"),
+    path("search/", search_views.search_results_page, name="search_results"),
 
     # Search API endpoints
-    path("api/search/", views.search_api, name="search_api"),
-    path("api/search/fast/", views.search_fast_api, name="search_fast_api"),
-    path("api/search/health/", views.search_health, name="search_health"),
+    path("api/search/", search_views.search_api, name="search_api"),
+    path("api/search/fast/", search_views.search_fast_api, name="search_fast_api"),
+    path("api/search/health/", search_views.search_health, name="search_health"),
 ]
